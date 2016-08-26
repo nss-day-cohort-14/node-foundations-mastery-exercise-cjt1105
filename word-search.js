dac31ee15338ac151cbe060c1e797d694d024fc1
CJT1105 <CJT1105@GMAIL.COM>
2016-08-26 13:14:38 -0500

    finished all base goals

--- word-search.js
+++ word-search.js
@@ -7,18 +7,24 @@ transform = require('stream').Transform,
 es = require('event-stream'),
 [,,args] = process.argv,
 stream = fs.createReadStream('/usr/share/dict/words','UTF8');
-
+console.log(args)
 let data = "",
 count = null;
 
-stream.pipe(es.split())
-.pipe(es.map((item, cb) => {
-	let word = item.toString();
-	let letters = word.substring(0, args.length);
-	if (letters.match(args)&& count <= 10) {
-		count ++;
-		cb(null, `${word}\n`);
-	}
-	cb();
-}))
-.pipe(process.stdout)
+if(args === undefined){
+	process.stdout.write(`Usage: 10.js [search term]\n`)
+}
+
+if(args != undefined){
+	stream.pipe(es.split())
+	.pipe(es.map((item, cb) => {
+		let word = item.toString();
+		let letters = word.substring(0, args.length);
+		if (letters.match(args)&& count <= 10) {
+			count ++;
+			cb(null, `${word}\n`);
+		}
+		cb();
+	}))
+	.pipe(process.stdout)
+}
