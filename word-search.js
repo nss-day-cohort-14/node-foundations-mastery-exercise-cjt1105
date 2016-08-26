9d4d17631ddfdfa342bcc91ce3ccf41fb8727316
CJT1105 <CJT1105@GMAIL.COM>
2016-08-26 13:05:40 -0500

    finished base goals

--- word-search.js
+++ word-search.js
@@ -2,16 +2,23 @@
 
 "use strict"
 
-const fs = require('fs');
+const fs = require('fs'),
+transform = require('stream').Transform,
+es = require('event-stream'),
+[,,args] = process.argv,
+stream = fs.createReadStream('/usr/share/dict/words','UTF8');
 
-const readStream = fs.createReadStream('/usr/share/dict/words');
-let data = "";
+let data = "",
+count = null;
 
-readStream.on('data',(chunk)=>{
-	data += `chunk:${chunk}`;
-});
-
-readStream.on('end',(doc)=>{
-		console.log(data)
-
-})
+stream.pipe(es.split())
+.pipe(es.map((item, cb) => {
+	let word = item.toString();
+	let letters = word.substring(0, args.length);
+	if (letters.match(args)&& count <= 10) {
+		count ++;
+		cb(null, `${word}\n`);
+	}
+	cb();
+}))
+.pipe(process.stdout)
