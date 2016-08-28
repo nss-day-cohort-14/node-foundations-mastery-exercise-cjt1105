7ba9daabad77e0c21451119f7b55e21e30a369a7
CJT1105 <CJT1105@GMAIL.COM>
2016-08-28 18:49:21 -0500

    added comments

--- word-search.js
+++ word-search.js
@@ -1,20 +1,22 @@
 #!/usr/bin/env node
 
 "use strict"
-
+// set up inital constants
 const fs = require('fs'),
 es = require('event-stream'),
 [,,searchTerm] = process.argv,
 stream = fs.createReadStream('/usr/share/dict/words','UTF8'),
 limitTen = require('./limit-ten').tStream;
-
+//check if user entered search term
 if(searchTerm === undefined){
 	process.stdout.write(`Usage: 10.js [search term]\n`)
 }
-
+// if search term entered
 if(searchTerm != undefined){
+	//Open read stream to read dictionary terms
 	stream
 	.pipe(es.split())
+	//find words that begin with search term
 	.pipe(es.map((item, cb) => {
 		let word = item.toString();
 		let letters = word.substring(0, searchTerm.length);
@@ -24,6 +26,7 @@ if(searchTerm != undefined){
 			cb();
 		}
 	}))
+	//get first ten resuts and write
 	.pipe(limitTen)
 	.pipe(process.stdout)
 }
